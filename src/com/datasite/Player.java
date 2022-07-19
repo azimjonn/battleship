f10a0046423dded9c8984bfe4055a40acaa065d5
Azimjon <azimjon.ibn.mehmonali@gmail.com>
2022-07-19 08:33:11 +0500

    !

--- src/com/datasite/Player.java
+++ src/com/datasite/Player.java
@@ -76,7 +76,6 @@ public class Player extends Thread{
 
         while (scA.hasNextLine()) {
             stringBuilder.append(scA.nextLine());
-            stringBuilder.append('\n');
             stringBuilder.append(scB.nextLine());
             stringBuilder.append('\n');
         }
